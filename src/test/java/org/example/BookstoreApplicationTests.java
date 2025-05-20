01e74d60d4b4ffcbe16cef6143c047f91ff98d0e
Maksymilian <mastu2115@gmail.com>
2025-05-20 17:24:08 +0200

    Added all required dependencies for Bookstore plugin.

---
+++ src/test/java/org/example/BookstoreApplicationTests.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test verifies that the Spring application context loads successfully
+    }
+
+    @Test
+    void simpleTest() {
+        // A simple test to verify JUnit is working
+        assert(true);
+    }
+}
